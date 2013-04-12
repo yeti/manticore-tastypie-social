aaf51935b68a49d775f7e6ef70c12a4b001e2beb
Rudy <rudy@yetihq.com>
2013-04-12 16:04:20 -0700

    comment resource and model

--- manticore_tastypie_social/models.py
+++ manticore_tastypie_social/models.py
@@ -1,6 +1,13 @@
 import re
+from django.conf import settings
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models.signals import post_save
+from mezzanine.accounts import get_profile_model
 from manticore_django.manticore_django.models import CoreModel
+
+# UserProfile = get_profile_model()
 
 
 class Tag(CoreModel):
@@ -10,7 +17,7 @@ class Tag(CoreModel):
         return u"%s" % self.name
 
 
-# Expects tags is stored in a field called 'related_tags'
+# Expects tags is stored in a field called 'related_tags' on implementing model and it has a parameter called TAG_FIELD to be parsed
 def relate_tags(sender, **kwargs):
     if kwargs['update_fields'] and 'related_tags' not in kwargs['update_fields']:
         return
@@ -34,3 +41,29 @@ def relate_tags(sender, **kwargs):
 
     if changed:
         kwargs['instance'].save()
+
+
+class Comment(CoreModel):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey()
+
+    description = models.CharField(max_length=120)
+    user_profile = models.ForeignKey(settings.AUTH_PROFILE_MODULE)
+
+    class Meta:
+        ordering = ['-created']
+
+#TODO: Add @ mention notifications
+# def comment_post_save(sender, **kwargs):
+#     if kwargs['created']:
+#         comment = kwargs['instance']
+#         for word in comment.description.split():
+#             if word.startswith("@"):
+#                 try:
+#                     user_profile = UserProfile.objects.get(user__username=word.replace("@", ""))
+#                     create_notification(user_profile, comment.user, comment.blurt.pk, Notification.TYPE_MENTION)
+#                 except UserProfile.DoesNotExist:
+#                     pass
+#
+# post_save.connect(comment_post_save, sender=Comment)
--- manticore_tastypie_social/resources.py
+++ manticore_tastypie_social/resources.py
@@ -1,5 +1,7 @@
+from tastypie.authorization import Authorization
 from manticore_tastypie_core.manticore_tastypie_core.resources import ManticoreModelResource
-from manticore_tastypie_social.manticore_tastypie_social.models import Tag
+from manticore_tastypie_social.manticore_tastypie_social.models import Tag, Comment
+from manticore_tastypie_user.manticore_tastypie_user.authentication import ExpireApiKeyAuthentication
 
 
 class TagResource(ManticoreModelResource):
@@ -10,3 +12,29 @@ class TagResource(ManticoreModelResource):
         resource_name = "tag"
         object_name = "tag"
         include_resource_uri = False
+
+
+class CommentResource(ManticoreModelResource):
+
+    class Meta:
+        queryset = Comment.objects.all()
+        allowed_methods = ['post']
+        authorization = Authorization()
+        authentication = ExpireApiKeyAuthentication()
+        resource_name = "comment"
+        always_return_data = True
+        object_name = "comment"
+
+    #TODO: notify user who owns the object being commented on and other commenters
+    # def obj_create(self, bundle, request=None, **kwargs):
+    #     bundle = super(CommentResource, self).obj_create(bundle, request, user=request.user.get_profile())
+    #
+    #     users = [bundle.obj.blurt.user]
+    #     for comment in bundle.obj.blurt.comment_set.all():
+    #         if comment.user not in users:
+    #             users.append(comment.user)
+    #
+    #     for user in users:
+    #         create_notification(user, bundle.obj.user, bundle.obj.blurt.pk, Notification.TYPE_COMMENT)
+    #
+    #     return bundle
