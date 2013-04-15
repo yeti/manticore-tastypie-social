e4e65f27d8bd244f85165ff6982bbe8d23d2b3ef
Rudy <rudy@yetihq.com>
2013-04-14 17:39:13 -0700

    fixing comment and imports

--- manticore_tastypie_social/utils.py
+++ manticore_tastypie_social/utils.py
@@ -1,9 +1,7 @@
-
-
-# Registers this library's resources
 from manticore_tastypie_social.manticore_tastypie_social.resources import TagResource, FollowResource
 
 
+# Registers this library's resources
 def register_api(api):
     api.register(TagResource())
     api.register(FollowResource())
