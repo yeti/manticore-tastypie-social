ee7794441058df845fe54796f14069940cf4d244
Rudy <rudy@yetihq.com>
2013-04-17 20:02:34 -0700

    function for sharing on social media

--- manticore_tastypie_social/utils.py
+++ manticore_tastypie_social/utils.py
@@ -1,3 +1,9 @@
+import urllib
+import urllib2
+from django.conf import settings
+import oauth2
+from social_auth.db.django_models import UserSocialAuth
+from tastypie.exceptions import BadRequest
 from manticore_tastypie_social.manticore_tastypie_social.resources import TagResource, FollowResource
 
 
@@ -6,3 +12,36 @@ def register_api(api):
     api.register(TagResource())
     api.register(FollowResource())
     return api
+
+
+def post_social_media(user, message, provider, raise_error=False):
+    try:
+        user_social_auth = UserSocialAuth.objects.get(user=user, provider=provider)
+
+        if user_social_auth.provider == 'facebook':
+            url = "https://graph.facebook.com/%s/feed" % user_social_auth.uid
+
+            params = {
+                'access_token': settings.FACEBOOK_APP_ACCESS_TOKEN,
+                'message': message
+                # 'link': bundle.obj.url
+            }
+
+            req = urllib2.Request(url, urllib.urlencode(params))
+            urllib2.urlopen(req)
+        elif user_social_auth.provider == 'twitter':
+            data = {
+                'status': message,
+                'wrap_links': True,
+                }
+
+            url = "https://api.twitter.com/1.1/statuses/update.json"
+            consumer = oauth2.Consumer(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET)
+            token = oauth2.Token(user_social_auth.tokens['oauth_token'], user_social_auth.tokens['oauth_token_secret'])
+            client = oauth2.Client(consumer, token)
+            client.request(url, 'POST', urllib.urlencode(data))
+        elif raise_error:
+            raise BadRequest("Does not support this provider: %s" % provider)
+    except (UserSocialAuth.DoesNotExist, urllib2.HTTPError, ValueError, IOError), e:
+        if raise_error:
+            raise e
